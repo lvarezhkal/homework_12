5395f1de3dd1d87c397bc036db734349a2317e4f
lvarezhkal <kkakxaa@gmail.com>
2024-01-23 19:47:58 +0300

    objectMethods

--- src/Book.java
+++ src/Book.java
@@ -20,8 +20,9 @@ public class Book {
     public void setYear (int year) {
         this.year = year;
     }
+
     public String toString(){
-        return name + " " + year;
+        return name + " " + author + " " + year;
     }
 
     @Override
